32a15cb7558352b976c3d4dd48be0a5244d9ad1a
Jeremiah <46662314+UCYT5040@users.noreply.github.com>
2021-09-03 17:49:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+# Get Enviroments
+token = os.getEnv("discord-bot-token")
+url = os.getEnv("db-url")
+password = os.getEnv("db-password")
+# Import Hikari
+import hikari, lightbulb
+# Setup DB
+# SOON
